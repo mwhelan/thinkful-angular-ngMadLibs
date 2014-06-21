5a7da7b72827e6aa1e26cf78a0cf39a892328860
Michael Whelan <mjmwdev@gmail.com>
2014-06-21 10:43:40 +0100

    added gender choice

--- assets/js/WordController.js
+++ assets/js/WordController.js
@@ -1,14 +1,32 @@
 ﻿angular.module("app")
-    .controller("WordController", function ($scope) {
-    $scope.data = {
-        femaleName: 'female name',
-        jobTitle: 'job title',
-        tediousTask: 'tedious task',
-        dirtyTask: 'dirty task',
-        celebrity: 'celebrity',
-        uselessSkill: 'useless skill',
-        obnoxiousCelebrity: 'obnoxious celebrity',
-        hugeNumber: 'huge number',
-        adjective: 'adjective'
-    };
+    .controller("WordController", function($scope) {
+        $scope.data = {
+            personName: 'person name',
+            jobTitle: 'job title',
+            tediousTask: 'tedious task',
+            dirtyTask: 'dirty task',
+            celebrity: 'celebrity',
+            uselessSkill: 'useless skill',
+            obnoxiousCelebrity: 'obnoxious celebrity',
+            hugeNumber: 'huge number',
+            adjective: 'adjective'
+        };
+    $scope.gender = new Gender(true);
 });
+
+function Gender(isFemale) {
+    this.isFemale = isFemale;
+
+    this.himHer = function () {
+        return this.isFemale ? "her" : "him";
+    }
+
+    this.heShe = function () {
+        return this.isFemale ? "she" : "he";
+    }
+
+    this.hisHer = function () {
+        return this.isFemale ? "her" : "his";
+    }
+
+}
