df5054a8df2d30728f85615bae05b47221fc23ca
Michael Whelan <mjmwdev@gmail.com>
2014-06-21 07:16:02 +0100

    added module and controller

---
+++ assets/js/WordController.js
@@ -0,0 +1,14 @@
+﻿angular.module("app")
+    .controller("WordController", function ($scope) {
+    $scope.data = {
+        femaleName: 'female name',
+        jobTitle: 'job title',
+        tediousTask: 'tedious task',
+        dirtyTask: 'dirty task',
+        celebrity: 'celebrity',
+        uselessSkill: 'useless skill',
+        obnoxiousCelebrity: 'obnoxious celebrity',
+        hugeNumber: 'huge number',
+        adjective: 'adjective'
+    };
+});
