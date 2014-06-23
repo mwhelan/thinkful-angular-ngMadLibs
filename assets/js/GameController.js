b29bedcf9ec0d056a9c407ecf14c0a882cfbf262
Michael Whelan <mjmwdev@gmail.com>
2014-06-23 14:11:39 +0100

    moved data entry into form with validation

---
+++ assets/js/GameController.js
@@ -0,0 +1,67 @@
+﻿angular.module("app")
+    .controller("GameController", function($scope) {
+        initializeForm();
+
+        $scope.submit = function () {
+            $scope.submitted = true;
+            if ($scope.myForm.$valid) {
+                $scope.view = "story";
+            }
+        };
+
+    $scope.submitted = false;
+
+        $scope.reset = function() {
+            initializeForm();
+        };
+
+        $scope.hasError = function (field, validation) {
+            if (!$scope.submitted) {
+                return false;
+            }
+
+            if (validation === undefined) {
+                validation = "required";
+            }
+
+            return $scope.myForm[field].$error[validation];
+        };
+
+        function initializeForm() {
+            $scope.data = getNewPerson();
+            $scope.gender = new Gender(true);
+
+            $scope.view = "form"; // views are 'form' and 'story'
+        }
+
+        function getNewPerson() {
+            return {
+                personName: '',
+                jobTitle: '',
+                tediousTask: '',
+                dirtyTask: '',
+                celebrity: '',
+                uselessSkill: '',
+                obnoxiousCelebrity: '',
+                hugeNumber: null,
+                adjective: ''
+            };
+        }
+});
+
+function Gender(isFemale) {
+    this.isFemale = isFemale;
+
+    this.himHer = function() {
+        return this.isFemale ? "her" : "him";
+    };
+
+    this.heShe = function() {
+        return this.isFemale ? "she" : "he";
+    };
+
+    this.hisHer = function() {
+        return this.isFemale ? "her" : "his";
+    };
+
+}
